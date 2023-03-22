805f58faad4f6c6fe316029e91ca830992747fa5
Jun Hwang <sungjun94@gmail.com>
2023-03-22 19:26:25 -0400

    hello world

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: '1', title: 'First Post!', content: 'Hello!' },
+  { id: '2', title: 'Second Post', content: 'More text' }
+]
+/* state in createSlice is the data itself, not from store.js/configureStore */
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded(state, action) {
+      state.push(action.payload)
+    }
+  }
+})
+
+export const {postAdded} = postsSlice.actions
+
+export default postsSlice.reducer
